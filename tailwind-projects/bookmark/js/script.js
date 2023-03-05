76fd5318cf25b4f42c7212bf71dbd74821e95545
SJ-WJ <sirajwahaj.ibrahim@gmail.com>
2023-03-05 16:31:16 -0700

    Finishing project 6 by completing the mobile menu

--- tailwind-projects/bookmark/js/script.js
+++ tailwind-projects/bookmark/js/script.js
@@ -1,5 +1,9 @@
 const tabs = document.querySelectorAll('.tab');
 const panels = document.querySelectorAll('.panel');
+const btn = document.getElementById('menu-btn');
+const menu = document.getElementById('menu');
+const logo = document.getElementById('logo');
+
 
 // Tabs menu event listener
 tabs.forEach((tab) => tab.addEventListener('click', onTabClick));
@@ -26,4 +30,20 @@ function onTabClick(e) {
   // Activate a panel based on the target
   const classString = e.target.getAttribute('data-target');
   document.getElementById('panels').getElementsByClassName(classString)[0].classList.remove('hidden')
+}
+
+
+// Hamburger event listener
+btn.addEventListener('click', navToggle);
+
+function navToggle() {
+  btn.classList.toggle('open');
+  menu.classList.toggle('flex');
+  menu.classList.toggle('hidden');
+
+  if (menu.classList.contains('flex')) {
+    logo.setAttribute('src', './images/logo-bookmark-footer.svg');
+  } else {
+    logo.setAttribute('src', './images/logo-bookmark.svg');
+  }
 }
